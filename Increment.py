bed1ecba9c80a1ba5f9e190d6703321841779db4
Kaustav <kaustavkaran2015@gmail.com>
2025-01-09 10:23:35 +0530

    advanced fucntions added

---
+++ Increment.py
@@ -0,0 +1,11 @@
+def increment(number, increment_by=1):
+    """
+    Increment the given number by 1.
+
+    Parameters:
+    number (int or float): The number to be incremented.
+
+    Returns:
+    int or float: The incremented number.
+    """
+    return number + increment_by
--- main.py
+++ main.py
@@ -1,37 +1,57 @@
+import Addition
+import Subtraction
+import Division
+import Multiplication
+import Increment
+import Decrement
+
 print("Select operation.")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 print("5. Power")
+print("6. Increment")
+print("7. Decrement")
 
-import Addition
-import Subtraction
-import Division
-import Multiplication
+operation = input("Enter choice (1/2/3/4/5/6/7): ")
 
-operation = input("Enter choice (1/2/3/4/5): ")
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: ")) 
-
-if operation == '1':
-    result = Addition.add(num1, num2)
-    print(f"The result of addition is: {result}")
-elif operation == '2':
-    result = Subtraction.subtract(num1, num2)
-    print(f"The result of subtraction is: {result}")
-elif operation == '3':
-    result = num1 * num2
-    print(f"The result of multiplication is: {result}")
-elif operation == '4':
-    if num2 != 0:
-        result = num1 / num2
-        print(f"The result of division is: {result}")
-    else:
-        print("Error! Division by zero.")
-elif operation == '5':
-    result = num1 ** num2
-    print(f"The result of power is: {result}")
-else:
-    print("Invalid input! Please select a valid operation.")
+match operation:
+    case '1':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: ")) 
+        result = Addition.add(num1, num2)
+        print(f"The result of addition is: {result}")
+    case '2':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: ")) 
+        result = Subtraction.subtract(num1, num2)
+        print(f"The result of subtraction is: {result}")
+    case '3':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: ")) 
+        result = num1 * num2
+        print(f"The result of multiplication is: {result}")
+    case '4':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: ")) 
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result of division is: {result}")
+        else:
+            print("Error! Division by zero.")
+    case '5':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter raise to: ")) 
+        result = num1 ** num2
+        print(f"The result of power is: {result}")
+    case '6':
+        num1 = float(input("Enter number: "))
+        result = Increment.increment(num1)
+        print(f"The result of increment is: {result}")
+    case '7':
+        num1 = float(input("Enter number: "))
+        result = Decrement.decrement(num1)
+        print(f"The result of decrement is: {result}")
+    case _:
+        print("Invalid input! Please select a valid operation.")
